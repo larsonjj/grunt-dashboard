c8310c7e5672c16d3d86c263830ea5354a1b39e4
Jake Larson <jake.j.larson@gmail.com>
2015-06-02 13:58:50 -0500

    Fixed formatting to be 2 spaces. Updated dashboard template to mimic the Yeogurt dashboard

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,108 +9,108 @@
 'use strict';
 
 module.exports = function(grunt) {
-    // load all npm grunt tasks
-    require('load-grunt-tasks')(grunt);
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                '<%= nodeunit.tests %>'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            }
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    dashboard: {
+      default_options: {
+        options: {
+          dashTemplate: 'dashboard/dashboard-template.hbs',
+          htmlTemplate: 'dashboard/html-template.hbs',
+          assets: [{
+            cwd: 'dashboard/assets/',
+            src: [
+              '**/*'
+            ]
+          }]
         },
-
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp']
-        },
-
-        // Configuration to be run (and then tested).
-        dashboard: {
-            default_options: {
-                options: {
-                    dashTemplate: 'dashboard/dashboard-template.hbs',
-                    htmlTemplate: 'dashboard/html-template.hbs',
-                    assets: [{
-                        cwd: 'dashboard/assets/',
-                        src: [
-                            '**/*'
-                        ]
-                    }]
-                },
-                files: {
-                    'dashboard/generated/dashboard_default.html': ['test/fixtures/default/*.html']
-                }
-            },
-            custom_options: {
-                options: {
-                    searchTerm: 'custom',
-                    dashTemplate: 'dashboard/dashboard-template.hbs',
-                    htmlTemplate: 'dashboard/html-template.hbs',
-                    assets: [{
-                        cwd: 'dashboard/assets/',
-                        src: [
-                            '**/*'
-                        ]
-                    }]
-                },
-                files: {
-                    'dashboard/generated/dashboard_custom.html': ['test/fixtures/custom/*.html']
-                }
-            },
-            jade: {
-                options: {
-                    dashTemplate: 'dashboard/dashboard-template.hbs',
-                    htmlTemplate: 'dashboard/html-template.hbs',
-                    assets: [{
-                        cwd: 'dashboard/assets/',
-                        src: [
-                            '**/*'
-                        ]
-                    }]
-                },
-                files: {
-                    'dashboard/generated/dashboard_jade.html': ['test/fixtures/jade/*.jade']
-                }
-            },
-            swig: {
-                options: {
-                    dashTemplate: 'dashboard/dashboard-template.hbs',
-                    htmlTemplate: 'dashboard/html-template.hbs',
-                    assets: [{
-                        cwd: 'dashboard/assets/',
-                        src: [
-                            '**/*'
-                        ]
-                    }]
-                },
-                files: {
-                    'dashboard/generated/dashboard_swig.html': ['test/fixtures/swig/*.swig']
-                }
-            }
-        },
-
-        // Unit tests.
-        nodeunit: {
-            tests: ['test/*_test.js']
+        files: {
+          'dashboard/generated/dashboard_default.html': ['test/fixtures/default/*.html']
         }
+      },
+      custom_options: {
+        options: {
+          searchTerm: 'custom',
+          dashTemplate: 'dashboard/dashboard-template.hbs',
+          htmlTemplate: 'dashboard/html-template.hbs',
+          assets: [{
+            cwd: 'dashboard/assets/',
+            src: [
+              '**/*'
+            ]
+          }]
+        },
+        files: {
+          'dashboard/generated/dashboard_custom.html': ['test/fixtures/custom/*.html']
+        }
+      },
+      jade: {
+        options: {
+          dashTemplate: 'dashboard/dashboard-template.hbs',
+          htmlTemplate: 'dashboard/html-template.hbs',
+          assets: [{
+            cwd: 'dashboard/assets/',
+            src: [
+              '**/*'
+            ]
+          }]
+        },
+        files: {
+          'dashboard/generated/dashboard_jade.html': ['test/fixtures/jade/*.jade']
+        }
+      },
+      swig: {
+        options: {
+          dashTemplate: 'dashboard/dashboard-template.hbs',
+          htmlTemplate: 'dashboard/html-template.hbs',
+          assets: [{
+            cwd: 'dashboard/assets/',
+            src: [
+              '**/*'
+            ]
+          }]
+        },
+        files: {
+          'dashboard/generated/dashboard_swig.html': ['test/fixtures/swig/*.swig']
+        }
+      }
+    },
 
-    });
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
 
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  });
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'dashboard', 'nodeunit']);
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'dashboard', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
 
 };
